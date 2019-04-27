aaed82b536fbf17d23ad19e30b9a0faed0e9d3a1
cherrycokeboy <schaga01@luther.edu>
2019-04-26 22:19:57 -0500

    got rid of something

--- twittere.py
+++ twittere.py
@@ -148,9 +148,7 @@ def main():
                 print('Active in',i+1,'mintues')
                 time.sleep(60)
             
-auth = tweepy.OAuthHandler('wklaEQfhDVGXfy8rpfw1ko8tX' , 'w2eDqOgQGSWfWJASHM7SBYXTWspDi9NiIvkTlUrz4FW8i1aGMV' )
-auth.set_access_token('1013820252-0uNb7vD9oY5YObc3KsyrhNdoRmyzRKQytXpZk0i', 'NnsjS4vXMccCuyEPgTdURj63MeVXLtCtJLncNjX3cy0LQ' )
-api = tweepy.API(auth) 
+
 
 #get_company()
 #with open('tweets.csv','wb') as f:
